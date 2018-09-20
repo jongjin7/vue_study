a100330302c9d3d18aa80d1b886eadb502d1db92
imjongjin <ezcode2875@gmail.com>
2018-09-20 23:39:43 +0900

    git start

---
+++ vue_cli/src/components/Example.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">Panel heading without title</div>
+    <div class="panel-body">
+      Panel content
+      <a href="/">메인으로</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Example",
+  data() {
+    return {
+      msg: "Basic panel example"
+    };
+  }
+};
+</script>
---
+++ vue_cli/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+
+      <li v-for="list in linklist">
+        <router-link :to="list.link">{{ list.content}}</router-link>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      linklist:[
+
+        {content: '서브페이지', link: '/sub'},
+
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
