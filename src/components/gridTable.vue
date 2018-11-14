6a34f9e07d2b67dda709c735ae2b5dc49d069638
imjongjin <ezcode2875@gmail.com>
2018-11-15 03:25:10 +0900

    앞으로 진행할 컴포넌트 정리

--- src/components/gridTable.vue
+++ src/components/gridTable.vue
@@ -190,7 +190,6 @@
 import $ from "jquery";
 import "datatables.net-bs4/css/dataTables.bootstrap4.css";
 import "datatables.net-bs4/js/dataTables.bootstrap4.js";
-import "../vendors/buttons.dataTables.min.css";
 
 export default {
   props: {
