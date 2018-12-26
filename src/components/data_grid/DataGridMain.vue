5ad5314c2ed0803ba0bab0bee9cbb1e8918a6468
jjlim <Poiu0987>
2018-12-26 19:43:12 +0900

    데이타 테이블 컴포넌트

--- src/components/data_grid/DataGridMain.vue
+++ src/components/data_grid/DataGridMain.vue
@@ -1,47 +1,36 @@
 <template>
   <div>
     <h1 class="mb-4">DataTableJs를 이용한 DataGrid</h1>
-    <div class="row">
-      <div class="col-12 col-md-9">
-          믹스인, 렌더함, 플러그인(할까말까)
-        필터는 검색폼에 적용 필요(커뮤니티)
-      </div>
-      <div class="col-12 col-md-3">
-
-      </div>
-    </div>
+    <datatable :columns="gridColumns" />
 
   </div>
 </template>
 
 <script>
 import datatable from './DataTable.vue';
-import 'datatables.net-bs4/js/dataTables.bootstrap4.js'
-// import the rest
-import 'datatables.net-buttons'
-import 'datatables.net-buttons-bs4'
-import 'datatables.net-select-bs4'
-import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css'
-import 'datatables.net-select-bs4/css/select.bootstrap4.min.css'
+
 
 export default {
   name: "grid",
   data() {
     return {
-
-    }
+      gridColumns: ['No.','Name','Email','Subject'],
+      gridData: ''
+    };
   },
   created(){
-
+    console.log('parent created')
   },
-  methods:{
 
+  mounted(){
+    console.log('parent mounted')
   },
   components: {
-    'v-server-table' : datatable
-  },
+    datatable
+  }
+  ,
+  methods:{
 
+  }
 };
-
-
 </script>
