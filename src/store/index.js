5e1366d36b7145ad18cd7eb40fa6e35e21a4c2e4
imjongjin <ezcode2875@gmail.com>
2019-01-03 09:25:18 +0900

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 // store.js
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 Vue.use(Vuex);
+
+import { SOME_MUTATION } from '../mutations/mutaion-types';
 
 export const store = new Vuex.Store({
   state: {
@@ -12,6 +13,21 @@ export const store = new Vuex.Store({
     popGalleryContent:null,
     latestGalleryItemIndex:'',
     communityTotalList:'',
+
+    count: 0,
+    myMessage:'',
   },
+  mutations: {
+    increment (state, payload) {
+      state.count += payload.amount
+    },
+    discrement(state){
+      state.count--
+    },
+    [SOME_MUTATION] (state, value) {
+      // 변이 상태
+      state.myMessage ='상수를 함수 이름으로 사용'+ value;
+    }
+  }
 
 });
